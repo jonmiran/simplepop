9c647ef85b13045f2afe57e466cf930d3626f705
gunnlaugursig@gmail.com <gunnlaugursig@gmail.com@e14f82a8-0807-11de-85a1-fd18384f7567>
2009-03-03 23:05:53 +0000

    smá viðbót

--- src/is/ru/tsam/pop3/server/SocketServer.java
+++ src/is/ru/tsam/pop3/server/SocketServer.java
@@ -8,13 +8,20 @@ package is.ru.tsam.pop3.server;
  *
  */
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServer extends Thread {
+
+	private static BufferedReader input;
+
+	private static BufferedWriter output;
+
+	private static Socket client = null;
 
 	/* Time out */
 	final static int TIME_OUT = 1;
@@ -40,15 +47,53 @@ public class SocketServer extends Thread {
 		while (true) {
 			try {
 				System.out.println(SocketServer.class.getSimpleName() + " waiting for connection on TCP port " + TCP_PORT);
-				Socket s = ss.accept();
-				BufferedReader is = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				String name = is.readLine();
-				String passwd = is.readLine();
-				String domain = is.readLine();
-				PrintWriter pout = new PrintWriter(s.getOutputStream(), true);
-				pout.println("Welcome to " + domain + ", " + name);
+				client = ss.accept();
+
+				input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
 			} catch (IOException e) {
 				System.err.println("Doh! " + e);
+			}
+
+			sendClientString("+OK Welcome to a POP3Server in java.");
+		}
+	}
+
+	public void output(String output) {
+		System.out.println(output);
+	}
+
+	public void sendClientString(String theString) {
+		try {
+			output.write(theString + "\r\n");
+			output.flush();
+
+			if(client!=null) {
+				output(client.getRemoteSocketAddress() + " < " + theString);
+			}
+		} catch (IOException e) {
+			if(client!=null) {
+				StackTraceElement[] element = e.getStackTrace();
+				output( client.getRemoteSocketAddress() + " IOException writing to client -> " + element[0]);
+			}
+			//reset it all after the error
+			resetConnection();
+		}
+	}
+
+	private void resetConnection() {
+		//close up
+		try {
+			if(client!=null) {
+				client.close();
+				client=null;
+			}
+		} catch(IOException e) {
+			if(client!=null) {
+				StackTraceElement[] element = e.getStackTrace();
+				output( client.getRemoteSocketAddress() + " Exception while closing. \n" + element[0]);
+				client=null;
 			}
 		}
 	}
